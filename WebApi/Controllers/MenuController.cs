43c42904d6a92647a6da35197e7b0a30445d9095
Jmurga16 <murga.jose16@gmail.com>
2023-03-30 04:15:15 -0500

    Creacion Endpoint de Menu

---
+++ WebApi/Controllers/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using UseCasesDTOs.Menu.ListMenu;
+using UseCasesPorts.Menu.ListMenu;
+using Microsoft.AspNetCore.Mvc;
+using Presenters.Menu.ListMenu;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : Controller
+    {
+
+        readonly IListMenuInputPort ListMenuInputPort;
+        readonly IListMenuOutputPort ListMenuOutputPort;
+
+
+        public MenuController(IListMenuInputPort listMenuInputPort, IListMenuOutputPort listMenuOutputPort) =>
+        (ListMenuInputPort, ListMenuOutputPort) = (listMenuInputPort, listMenuOutputPort);
+
+
+        [HttpGet]
+        public async Task<IEnumerable<ListMenuResponse>> GetMenus()
+        {
+
+            await ListMenuInputPort.GetMenus();
+
+            var Presenter = ListMenuOutputPort as ListMenuPresenter;
+
+            return Presenter.Content;
+        }
+    }
+}
